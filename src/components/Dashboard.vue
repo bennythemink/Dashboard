60f8ada0b22e47b12cceb96c67bcda47cd11b81b
Benny Sheerin <bennysheerin@yahoo.ie>
2019-02-20 21:52:40 +1100

    Adding initial structure

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>Dashboard</h1>
+        <Utils />
+    </div>
+</template>
+
+<script>
+
+import Utils from '@/components/Utils.vue'
+
+export default {
+  name: 'dashboard',
+  components: {
+    Utils
+  },
+  data: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Utils.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h2>Utils</h2>
+        <Happiness />
+    </div>
+</template>
+
+<script>
+
+import Happiness from '@/components/Happiness.vue'
+
+export default {
+  name: 'utils',
+  components: {
+    Happiness
+  },
+  data: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
